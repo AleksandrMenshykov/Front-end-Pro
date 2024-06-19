d83d413c6cfae2408f4713a17aa7c014c4b2f345
Aleksandr Menshykov <menshikovalexandr1992@gmail.com>
2024-06-19 16:52:57 +0300

    Выполнил ДЗ 3.1

---
+++ Homework_3/homework_3.1.js
@@ -0,0 +1,15 @@
+const userName = "Alex";
+const userAge = 32;
+const bigNumber = 4234324234324324324324324n;
+const isTrue = true;
+const variable;
+const isNull = null;
+const isSymbol = Symbol("description")
+
+console.log(typeof userName);  //string
+console.log(typeof userAge);   //number 
+console.log(typeof bigNumber); //biginit
+console.log(typeof isTrue);    //boolean
+console.log(typeof variable);  //undefined
+console.log(typeof isNull);    //object
+console.log(typeof isSymbol);  //symbol
