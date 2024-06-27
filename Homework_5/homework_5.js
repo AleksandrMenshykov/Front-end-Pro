87de15f77256da4a16eae6574c4819c8a14ca467
Aleksandr Menshykov <menshikovalexandr1992@gmail.com>
2024-06-27 17:38:55 +0300

    Віполнил дз 5.3

--- Homework_5/homework_5.js
+++ Homework_5/homework_5.js
@@ -1,32 +1,43 @@
-/*
-Homework 5.1
+// /*
+// Homework 5.1
 
-Вивести в консоль числа від 20 до 30 через пропуск використовуючи крок 0,5 (20 20,5 21 21,5 ....)
-*/
+// Вивести в консоль числа від 20 до 30 через пропуск використовуючи крок 0,5 (20 20,5 21 21,5 ....)
+// */
 
-for (let a = 20; a <= 30; a += 0.5) {
-  console.log(a, 'for')
-}
+// for (let a = 20; a <= 30; a += 0.5) {
+//   console.log(a, 'for')
+// }
 
-let b = 20
-while (b <= 30) {
-  console.log(b, 'while');
-  b += 0.5;
-}
+// let b = 20
+// while (b <= 30) {
+//   console.log(b, 'while');
+//   b += 0.5;
+// }
 
-let c = 20
-do {
-  console.log(c, 'do while');
-  c += 0.5;
-}
-while (c <= 30);
+// let c = 20
+// do {
+//   console.log(c, 'do while');
+//   c += 0.5;
+// }
+// while (c <= 30);
 
-/*ДЗ 5.2. Розрахунок валюти
+// /*ДЗ 5.2. Розрахунок валюти
 
-Один долар коштує 26 гривень. Вивести дані з розрахунком вартості 10, 20, 30... 100 доларів
-*/
+// Один долар коштує 26 гривень. Вивести дані з розрахунком вартості 10, 20, 30... 100 доларів
+// */
 
-for (let usa = 10; usa <= 100; usa += 10) {
-  let dollar = 26
-  console.log(dollar * usa);
+// for (let usa = 10; usa <= 100; usa += 10) {
+//   let dollar = 26
+//   console.log(dollar * usa);
+// }
+
+/* ДЗ 5.3. Пошук потрібних чисел
+
+Дано ціле число N (ввести через prompt). Вивести всі цілі числа від 1 до 100, квадрат яких не перевищує числа N */
+
+const questioPrompt = prompt("Введите число от 1 до 100")
+const promptToString = Number(questioPrompt)
+
+for (let a = 1; a <= promptToString; a++) {
+  console.log(a)
 }
