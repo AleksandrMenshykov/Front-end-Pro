d129a877c55ccdf31f9f55b07e1c584c328efaeb
Aleksandr Menshykov <menshikovalexandr1992@gmail.com>
2024-07-30 20:54:03 +0300

    Выполниз дз 6.3

--- Homework_6/Homework-6.js
+++ Homework_6/Homework-6.js
@@ -20,3 +20,31 @@ function culcSumm() {
 const summ = culcSumm()
 
 console.log(summ)
+
+/* 
+ДЗ 6.3. Функція видалення елементу
+
+Реалізуйте функцію removeElement(array, item), щоб видалити елемент item з масиву array.
+
+Наприклад:
+
+const array = [1, 3, 4, 6, 2, 5, 7];
+
+removeElement(array,4);
+
+console.log(array); // Результат: [1, 3, 6, 2, 5, 7] */
+
+const array = [1, 3, 4, 6, 2, 5, 7];
+
+
+function removeElement(array, item) {
+
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === item) {
+      array.splice(i, 1)
+    }
+  } console.log(array)
+
+}
+
+removeElement(array, 7)
