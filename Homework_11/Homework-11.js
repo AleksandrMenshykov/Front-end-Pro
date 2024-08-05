804492950f8852d1f21530bc0078abd95b136761
Aleksandr Menshykov <menshikovalexandr1992@gmail.com>
2024-08-05 20:28:12 +0300

    выполнил дз 11.1

--- Homework_11/Homework-11.js
+++ Homework_11/Homework-11.js
@@ -1,4 +1,32 @@
 /*
+ДЗ 11.1. Таблиця піфагора
+
+Вивести таблицю Піфагора (10×10), проте цього разу таблиця повинна бути створена динамічно
+*/
+
+const table = document.createElement('table')
+document.body.appendChild(table)
+table.style.padding = '10px'
+
+const tbody = document.createElement('tbody')
+table.appendChild(tbody)
+
+for (let i = 1; i <= 10; i++) {
+  const tr = document.createElement('tr')
+  tbody.appendChild(tr)
+
+  for (let j = 1; j <= 10; j++) {
+    const td = document.createElement('td')
+    tr.appendChild(td)
+    td.textContent = i * j
+    td.style.cssText = `
+      padding: 5px;
+      border: 1px solid black;
+    `
+  }
+}
+
+/*
 ДЗ 11.2. Кнопка зміни кольору
 
 Є блок із текстом на сторінці та кнопку. При натисканні на кнопку текст змінює колір.
