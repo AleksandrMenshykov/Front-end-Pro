e2da7b59fd3e6d6e5678a97667e31a2d148809b1
Aleksandr Menshykov <menshikovalexandr1992@gmail.com>
2024-08-30 23:52:48 +0300

    выполнил дз 7.1

--- Homework_7/Homework-7.js
+++ Homework_7/Homework-7.js
@@ -1,3 +1,33 @@
+/* Написати функцію, яка приймає 1 параметр. Та скадае значення з тим, що передали перший раз і т. д. 
+Все це із замиканнями, наприклад:
+
+console.log(sum(4)); // 4
+
+console.log(sum(6)); // 10
+
+console.log(sum(10)); // 20
+
+console.log(sum(7)); // 27 */
+
+
+function calculateSum() {
+  let sum = 0;
+  return function (value) {
+    sum = sum + value;
+    return sum
+  }
+}
+
+const sum = calculateSum()
+
+console.log(sum(4))
+console.log(sum(6))
+console.log(sum(10))
+console.log(sum(7))
+
+
+
+
 /*
 ДЗ 7.3. Функція з циклом
 
