342f25f9c28bbf5dc7812cd6328cb075c5d31256
Aleksandr Menshykov <menshikovalexandr1992@gmail.com>
2024-10-13 22:19:45 +0300

    Homework 17.2

--- Homework_17/Homework-17.js
+++ Homework_17/Homework-17.js
@@ -39,3 +39,29 @@ console.log(calc.multiply(3, 6)); // 18
 
 console.log(calc.divide(8, 2)); // 4
 
+/*
+ДЗ 17.2. Клас "Тренер"
+
+Створіть клас Coach, який буде представляти тренера. Додайте властивості, такі як ім'я, спеціалізація та рейтинг.
+Також реалізуйте метод для виведення інформації про тренера та його рейтинг.
+*/
+
+class Coach {
+
+  constructor(name, specialization, rating) {
+    this.name = name;
+    this.specialization = specialization;
+    this.rating = rating;
+  }
+
+  displayInfo() {
+    console.log(`Coach: ${this.name}, Specialization: ${this.specialization}, Rating: ${this.rating}`)
+  }
+}
+
+const coach1 = new Coach('John Doe', 'Fitness', 4.7);
+
+console.log(coach1)
+coach1.displayInfo()
+
+
